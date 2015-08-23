08bc2b098fb44b8408a78aad1c785badbee68406
Alex Petrosh <petrosh@Mac-Pro-di-petrosh.local>
2015-08-23 14:50:27 +0200

    read config and list name commits

--- js/script.js
+++ js/script.js
@@ -19,22 +19,12 @@ if( localStorage.getItem("token") !== null ){
   var repo = github.getRepo(username, reponame);
   repo.read('master', 'config.json', function(err, data) {
     if(err===null){
-      // Append content in textarea
-      document.getElementById("config").value = data;
-      // Read commits
-      repo.getCommits({sha:'master', path:'config.json'}, function(err, data){
-        if(err===null){
-          // Commits loop
-          commitsList = document.getElementById("commits");
-          Object.keys(data).forEach(function(key,index){
-            if(index === 0) commitsList.removeChild(commitsList.firstChild);
-            // Create list item, populate and append
-            newLi = document.createElement("li");
-            newLi.innerHTML = '<a href="'+data[key].html_url+'">'+data[key].commit.message+'</a>';
-            commitsList.appendChild(newLi);
-          });
-        }else console.log(err);
-      },function(err){console.log(err);});
+      var config = JSON.parse(data.responseText);
+      localStorage.setItem("config",config);
+      // Append system in div
+      document.getElementById("config").value = config.system;
+      // Read `name.txt` commits
+      readCommits(repo);
     }else{
       // Display error and remove token from localStorage
       document.getElementsByTagName('section')[0].innerHTML = '<h1>error '+err.error+'</h1><a href="/'+reponame+'">Again</a>';
@@ -48,17 +38,35 @@ if( localStorage.getItem("token") !== null ){
     // Retrieve textarea content
     body = document.getElementById("body").value;
     // Write on master branch
-    repo.write('master', 'LOG.md', body, 'committed '+new Date(), function(err, sha) {
+    repo.write('master', 'data/name.txt', body, 'committed '+new Date(), function(err, sha) {
       if(err===null){
         // Success
         document.getElementsByTagName('section')[0].innerHTML = '<h1>Commit ok</h1><h2>sha '+sha+'</h2><a href="/'+reponame+'">Again</a>';
       }else{
         // Error, remove token
-        document.getElementsByTagName('section')[0].innerHTML = '<h1>error '+err.error+'</h1><a href="/'+reponame+'">Again</a>';
         localStorage.removeItem('token');
+        document.getElementsByTagName('section')[0].innerHTML = '<h1>error '+err.error+'</h1><a href="/'+reponame+'">Again</a>';
       }
     });
   },false);
 }else{
   window.location = '/login/';
+}
+function readCommits(repo){
+  // Read commits
+  repo.getCommits({sha:'master', path:'data/name.txt'}, function(err, data){
+    if(err===null){
+      // Commits loop
+      commitList = document.getElementById("commits");
+      Object.keys(data).forEach(function(key,index){
+        if(index === 0) commitList.removeChild(commitList.firstChild);
+        // Create list item, populate and append
+        newLi = document.createElement("li");
+        newLi.innerHTML = '<a href="'+data[key].html_url+'">'+data[key].commit.message+'</a>';
+        commitsList.appendChild(newLi);
+      });
+      // Append name in textarea
+      document.getElementById("body").value = data;
+    }else console.log(err);
+  });
 }
